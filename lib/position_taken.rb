572670b0123260548b56e9ae98723b48faae7790
Amari Ellington <amari.l.ellington@gmail.com>
2019-02-27 19:59:48 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,11 +1,4 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
-
-taken = nil
-  if board[index] == nil || board[index] == " " || board[index] == " "
-    taken = false
-  else
-    taken = true
-end
-
-taken
+  ! (board[index] ==  " " || board[index] == "" || board[index] == nil) 
+  end
